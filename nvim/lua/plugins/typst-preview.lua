a4c2ddf36e69a3e69e0b5c114531a2f8e630994c
Kieran <kieran.mang@gmail.com>
2026-01-25 15:08:26 +0000

    change browser for typst-preview.nvim to firefox

--- nvim/lua/plugins/typst-preview.lua
+++ nvim/lua/plugins/typst-preview.lua
@@ -1,8 +1,9 @@
 return {
-  'chomosuke/typst-preview.nvim',
-  lazy = false, -- or ft = 'typst'
-  version = '1.*',
-  opts = {
-    follow_cursor = false,
+    'chomosuke/typst-preview.nvim',
+    lazy = false, -- or ft = 'typst'
+    version = '1.*',
+    opts = {
+        follow_cursor = false,
+        open_cmd = "firefox %s",
     }, -- lazy.nvim will implicitly calls `setup {}`
 }
